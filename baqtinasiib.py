b8cd9f41a866a12a6ba9405f757182ae4dcb07d6
Mohamedahmeddev12 <myself40009@gmail.com>
2026-01-20 17:02:26 +0300

    Add files via upload

---
+++ baqtinasiib.py
@@ -0,0 +1,54 @@
+"""x=30
+y=80
+print(type(x*y))
+"""
+"""x=10
+y="mushaar:"
+print(y,x)"""
+magac=input("magacaaga koobaad:")
+magac2=input("magacaaga labaad:")
+magac3=input("magacaaga sadexaad:")
+print("hello welcome to",magac,magac2,magac3)
+nasiib=int(input("lambarkeed qabsaneesaa 1---20:"))
+if nasiib==17:
+    print("waxaad ku guuleysay kabo")
+elif nasiib==8:
+    print("waxaad ku guuleysatay shaar")
+elif nasiib==9:
+    print("waxaad ku guuleysatay saabuun")
+elif nasiib==7:
+    print("waxaad ku guuleysatay dhar")
+elif nasiib==6:
+    print("waxaad ku guuleysatay saacad")
+elif nasiib==4:
+    print("waxaad ku guuleysatay jaakad")
+elif nasiib==3:
+    print("waxaad ku guuleysatay hilib")
+elif nasiib==16:
+    print("waxaad ku guuleysatay lacag dhan: 150$")
+elif nasiib==2:
+    print("waxaad ku guuleysatay set perfume-ka shuyuuq")
+elif nasiib==1:
+    print("waxaad ku guuleysatay malab")
+elif nasiib==12:
+    print("waxaad ku guuleysatay Hal miilyan shiling somali")
+elif nasiib==11:
+    print("waxaad ku guuleysatay suit")
+elif nasiib==19:
+    print("waxaad ku guuleysatay marwaaxad")
+elif nasiib==5:
+    print("waxaad ku guuleysatay salaan")
+elif nasiib==15:
+    print("waxaad ku guuleysatay futashaari")
+elif nasiib==18:
+    print("waxaad ku guuleysatay feeraa dharka")
+elif nasiib==20:
+     print("barkin")
+elif nasiib==13:
+    print("waxaad ku guuleysatay frinkeer")
+elif nasiib==10:
+    print("Hambalyo waxaad ku guuleysatay  5GB oo data internet")
+elif nasiib==14:
+    print("waxaad ku guuletsatay cabitaanka zamam foster")
+else:
+    print("Fadlan dooro 1---20")
